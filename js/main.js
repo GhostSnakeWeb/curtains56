984aeacb0f6f8639a7e6e968c40066aaff8ee3c2
Александр Тихонов <oren.andrey@gmail.com>
2019-08-29 19:42:33 +0500

    Переделано меню

--- js/main.js
+++ js/main.js
@@ -1,6 +1,13 @@
 $(document).ready(function() {
 
-	/* Call plugins hrere ...*/
+	/* Call plugins here ...*/
+    $('.menu>ul>li').mouseenter(function () {
+        $(this).find('.dropdown').css("display",'block');
+        
+    })
 
+    $('.menu>ul>li').mouseleave(function () {
+        $(this).find('.dropdown').css("display",'none');
+    })
 	
 });
